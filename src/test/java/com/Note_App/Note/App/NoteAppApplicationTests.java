41fd3191fa323a1bdfcfa733f6eba3a81e366f8d
MihaiSpintecatorul76 <voicumihai49@gmail.com>
2025-09-18 12:39:55 +0300

    Initial commit

---
+++ src/test/java/com/Note_App/Note/App/NoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Note_App.Note.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
